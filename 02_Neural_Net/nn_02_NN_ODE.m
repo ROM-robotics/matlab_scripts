9dabfd0e158508781f8efe8d57432e9bb5436ba9
pyae soan aung <server01.psa1981@gmail.com>
2023-06-01 23:11:46 +0630

    nn

---
+++ 02_Neural_Net/nn_02_NN_ODE.m
@@ -0,0 +1,14 @@
+clear all;clc;
+t,y = sim("nn_02_simulink.slx");
+
+P = t';
+T = t.tout;
+T = T';
+%{
+net = feedforwardnet([7 4]);
+trained_net = train(net, P, T);
+
+A = sim(trained_net, P);
+gensim(trained_net, 0.01);
+%}
+t
---
+++ 02_Neural_Net/nn_03_NN_sin.m
@@ -0,0 +1,13 @@
+clear;clc;
+
+x = -1:0.05:1;
+y = sin(2*pi*x);
+% plot(x,y);
+
+net = feedforwardnet([8 5]);
+[trained_net, tr] = train(net,x,y);
+y_hat = sim(trained_net, x);
+figure(1);plot(x,y,"-",x,y_hat,'-');
+legend('actual','predict');
+grid on;
+
