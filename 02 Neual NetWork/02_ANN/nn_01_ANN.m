0ccb3e91f30339accef60def70e86a314e2e6af4
pyae soan aung <server01.psa1981@gmail.com>
2023-06-04 22:09:58 +0630

    nn

--- 02 Neual NetWork/02_ANN/nn_01_ANN.m
+++ 02 Neual NetWork/02_ANN/nn_01_ANN.m
@@ -1,76 +1,99 @@
 %%%% ANN ( default hardlimit activiation function ) %%%%
 clear;clc;
+
 %{ %% 1 Creating a new perceptron with newp()
-net = newp([0 1;-2 2],1);
-%%view(net);
-% default input weight = 0
-net.IW(1,1);
+% [-1 1;-2 2] က input နှစ်ခုရဲ့ range 
+% 1 က neuron အရေအတွက်, 
+% TF='Hardlim'သူက activation function 
+% LF='learnp' သူက learning algo
+
+net = newp([-1 1;-2 2],1);
+%view(net);
+
+% default weight တန်ဖိုးနှစ်ခုက 0
+%net.IW{1,1};
+net.IW{1,1}=[0.9,0.1];
+
 % default Layer weight
 net.LW;
 % default bias
 net.biases;
-% Training
-[net, tr] = train(net, P, T, Pi, Ai)
-% Simulation
-sim(net,X,Xi,Ai,T)
+
+X = [1 -1 1;
+     2 -2 2];
+Y = [3 -3 3];
+% Training ( train(all) or adapt(one) )
+% [ updated_network, epoch ] 
+[net, tr] = train(net, X, Y);
+
 %}
 
 
 %{ %% 2 AND gate perceptron
-P = [0 0 1 1;
+X = [0 0 1 1;
      0 1 0 1];
-y = [0 0 0 1];
+Y = [0 0 0 1];
 
 net = newp([0 1;0 1],1);
-net = train(net, P,y);
+trained_net = train(net, X, Y);
 
-%% After training , feed input data and simulate
-y_hat = sim(net, P);
+% After training , feed input data and simulate
+y_hat = sim(trained_net, X);
 %}
 
 
 %{ %% 3 Feedforward
 % defalut 1 hidden with 10 neurons, 1 output
-net = feedforwardnet();
-net = feedforwardnet(8);
+net1 = feedforwardnet();
+% 1 Hidden layers with 8 nodes
+net2 = feedforwardnet(8);
 % 2 Hidden layers with 8 nodes and 3 nodes
-net = feedforwardnet([8 3]);
-view(net);
+net3 = feedforwardnet([8 3]);
+view(net2);
 %}
 
 %{ %% 4 Function with one variable
 x = 0:0.05:2;
-y = 1./((x-0.3).^2 + 0.1)  + 1./((x-0.9).^2+0.04) - 6;
-%plot(x,y);
+y = 1./( (x-0.3).^3 + 0.1 )  + 1./( (x-0.9).^2+0.04 ) - 6;
+% plot(x,y);
 net = feedforwardnet([5 10]);
-net.trainParam.goal = 0.001;
-net1 = train(net,x,y);
-A = sim(net1,x)
-plot(x,y,x,A);
+net.trainParam.goal = 0.001; % performance goal
+trained_net = train(net,x,y);
+y_hat = sim(trained_net, x);
+figure(1);plot(x,y,"-",x,y_hat,'-');
+legend('actual','predict');
 %}
 
-%{  %% 5 Function with two variable
+%{ %% 5 Function with two variable
 x = -2:0.25:2;
 y = -2:0.25:2;
-[x1,y1] = meshgrid(x,y);
+[x1,y1] = meshgrid(x,y); % return 1x17 to 17x17
 z = sin(x1).*cos(y1);
-mesh(x,y,z);
-P = [x;y];
-T = z;
+% mesh(x,y,z); xlabel('x'); ylabel('y'); zlabel('z');
+X = [x;y];
+Y = z;
 net = feedforwardnet([20 15]);
 net.trainParam.goal = 0.0001;
-net1 = train(net,P,T);
-A = sim(net1,P);
+trained_net = train(net, X, Y);
+predict_output = sim(trained_net, X);
+%plot(x,z,'-',x,predict_output,'o');
+plot(y,z,'-',y,predict_output,'o');
 %}
 
 %{  %% 6 Function with multi variable
-a = rand(1,100); b = rand(1,100); c = rand(1,100); d = rand(1,100);
+a = rand(1,100); 
+b = rand(1,100); 
+c = rand(1,100); 
+d = rand(1,100);
+
 y = 3.*a + 4.*a.*b + 2.*c + d;
-P = [a;b;c;d]
-T = y;
+X = [a;b;c;d]
+Y = y;
 net = feedforwardnet([6 2]);
-net.trainParam.goal = 0.0001;
-net1 = train(net,P,T);
-A = sim(net1,P);
+net.trainParam.goal = 0.1;
+trained_net = train(net, X, Y);
+Y_predict = sim(trained_net, X);
+plot(X,Y,'-',X,Y_predict,'o');
+legend('actual','predict');
 %}
 %%%% ------------------------------------------------------------------------%%%%
--- 02 Neual NetWork/02_ANN/nn_02_NN_ODE.m
+++ 02 Neual NetWork/02_ANN/nn_02_NN_ODE.m
@@ -1,11 +1,14 @@
 %%%% ODE  %%%%
 clear; clc;
-t,y = sim("nn_02_simulink.slx");
+a = sim('nn_02_simulink.slx',20);
 
+
+a.plot;
+
+%{
 P = t';
 T = t.tout;
 T = T';
-%{
 net = feedforwardnet([7 4]);
 trained_net = train(net, P, T);
 
